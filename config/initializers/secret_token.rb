5ca6753e4fd3e3beb57e6abf698c5dbb8ed93186
Dominick <d.reinhold@yahoo.com>
2011-01-08 15:31:49 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaznooDeals::Application.config.secret_token = '4f4a4346538d7384f96d3b4f3a67cc3425287ec98b800eb014b7286243f479e191a1e23940e0ffb24953eeca19e69da691bd4aa0926c75fd64b0ea7007ab38f4'
