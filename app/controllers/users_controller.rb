973ae04169a1caff98507d391f9b2ebd19fae5ca
Dominick <d.reinhold@yahoo.com>
2011-01-10 23:51:39 -0600

    Done with user edit/update, index, and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update, :index]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => [ :index, :destroy ]
   
   def new
     @title = "Sign up"
     @user = User.new
+  end
+  
+  def edit
+    @title = "Settings"
   end
   
   def create
@@ -20,7 +27,52 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @title = @user.name
   end
 
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Settings updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+   def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+  
+ 
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+
+	def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      if current_user.nil?
+		deny_access
+	  else
+      must_be_admin unless current_user.admin?
+      end
+    end
+
+  
 end
+
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,8 +10,24 @@ module SessionsHelper
     self.current_user = nil
   end
 
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
   
-  
+  def must_be_admin
+    redirect_to root_path, :notice => "You must be an administrator to access this page."
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
   def current_user=(user)
     @current_user = user
   end
@@ -33,4 +49,14 @@ module SessionsHelper
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
     end
+    
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
+
+    
 end
